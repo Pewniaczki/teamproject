9504a2bc951e375240f67732409ed19f2a68678e
Marek <mareksax@wp.pl>
2024-11-15 17:35:02 +0100

    chore: setup gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/team_project_frontend/',
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
