b511853cd6995d2f333bf871be3d9ebf4be02563
Marek <mareksax@wp.pl>
2024-11-15 21:57:42 +0100

    chore: add stylelint config

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './scss/custom.scss';
 import App from './App.tsx';
+import './scss/custom.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  customSyntax: 'postcss-scss',
+  rules: {
+    'color-no-invalid-hex': true,
+  },
+};
