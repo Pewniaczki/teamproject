9eecac2e12adcf14b3597a673d79bc9a91807c97
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-01-21 23:14:08 +0100

    added current macthes

---
+++ src/components/CurrentMatches/CurrentMatches.tsx
@@ -0,0 +1,75 @@
+import styles from './CurrentMatches.module.scss';
+import SoccerBall from '../../assets/icons/soccer-ball.svg';
+import BarcelonaIcon from '../../assets/icons/barselonaSpain.svg'; 
+import ManchesterIcon from '../../assets/icons/ManchesterLondon.svg';
+
+export const CurrentMatches = () => {
+  const matches = [
+    {
+      teams: [
+        { name: 'Barcelona', icon: BarcelonaIcon },
+        { name: 'Manchester', icon: ManchesterIcon },
+      ],
+      time: '20:45',
+      day: 'Today',
+    },
+    {
+        teams: [
+            { name: 'Barcelona', icon: BarcelonaIcon },
+            { name: 'Manchester', icon: ManchesterIcon },
+          ],
+      time: '22:30',
+      day: 'Today',
+    },
+    {
+        teams: [
+            { name: 'Barcelona', icon: BarcelonaIcon },
+            { name: 'Manchester', icon: ManchesterIcon },
+          ],
+      time: '19:45',
+      day: 'Tomorrow',
+    },
+    {
+        teams: [
+            { name: 'Barcelona', icon: BarcelonaIcon },
+            { name: 'Manchester', icon: ManchesterIcon },
+          ],
+      time: '21:15',
+      day: 'Tomorrow',
+    },
+  ];
+
+  return (
+    <div className={styles.container}>
+      {matches.map((match, index) => (
+        <div key={index} className={styles.card}>
+          <div className={styles.teams}>
+            {match.teams[0].icon ? (
+              <img
+                src={match.teams[0].icon}
+                alt={match.teams[0].name}
+                className={styles.icon}
+              />
+            ) : (
+              <span>{match.teams[0].name}</span>
+            )}
+            <img src={SoccerBall} alt="soccer ball" className={styles.icon} />
+            {match.teams[1].icon ? (
+              <img
+                src={match.teams[1].icon}
+                alt={match.teams[1].name}
+                className={styles.icon}
+              />
+            ) : (
+              <span>{match.teams[1].name}</span>
+            )}
+          </div>
+          <div className={styles.info}>
+            <span>{match.day}</span>
+            <span>{match.time}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import { CountryMatches } from '../../components/CountryMatches';
+import { CurrentMatches } from '../../components/CurrentMatches/CurrentMatches';
 import { countryMatches } from '../../data/CountryMatches';
 import style from './HomePage.module.scss';
 
@@ -6,15 +7,22 @@ export const HomePage = () => {
   return (
     <div className={style.home}>
       <div className={style.home__list}>
+
+      <CurrentMatches />
+      
         {countryMatches.map((countryMatch) => {
           const { countryFlag, countryName, leagues } = countryMatch;
           return (
+            <>
+            
             <CountryMatches
               key={countryFlag}
               countryFlag={countryFlag}
               countryName={countryName}
               leagues={leagues}
             />
+            </>
+            
           );
         })}
       </div>
