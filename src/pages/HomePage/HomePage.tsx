58b98d7339520a2c76b1b685588dc82fa22e560f
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-01-30 17:16:40 +0100

    topMenu

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,10 @@
+import { TopMenu } from '../../components/TopMenu/TopMenu';
+
 export const HomePage = () => {
   return (
     <div>
       <p>Welcome to the Home Page</p>
+      <TopMenu />
     </div>
   );
 };
