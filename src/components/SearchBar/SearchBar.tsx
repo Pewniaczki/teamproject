5add209d9aa6079a1b2d96950b71c6d76546cb68
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-03-14 18:16:44 +0100

    feat: enhance SearchBar component with input field and search icon; update styles for improved layout

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,14 @@
 import styles from './SearchBar.module.scss';
 
 export const SearchBar: React.FC = () => {
-    return (
-        <div className={styles.searchBar}>
-            <p className={styles.searchBar__text}>Matches</p>
-        </div>
-    )
+  return (
+    <div className={styles.searchBar}>
+      <p className={styles.searchBar__text}>Matches</p>
+
+      <div className={styles.search}>
+        <img />
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
