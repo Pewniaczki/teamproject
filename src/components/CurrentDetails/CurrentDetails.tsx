2d9cc3d67e952aa42cfe6bfd32ec520ecf348da0
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-03-15 18:49:17 +0100

    feat: update CurrentDetails component layout and styles; add new block elements and adjust color variables for consistency

--- src/components/CurrentDetails/CurrentDetails.tsx
+++ src/components/CurrentDetails/CurrentDetails.tsx
@@ -5,30 +5,59 @@ export const CurrentDetails: React.FC = () => {
     <div className={styles.container}>
       <div className={styles.element}>
         <p className={styles.element_title}>Who will win</p>
-        <div className={styles.element_block}></div>
-        <div className={styles.element_block2}></div>
-        <p className={styles.element_paragraph}>Your prediction, your game - vote!</p>
+
+        <div className={styles.element_container}>
+          <div className={styles.element_block}></div>
+
+          <div className={styles.element_block2}></div>
+        </div>
+
+        <p className={styles.element_paragraph}>
+          Your prediction, your game - vote!
+        </p>
       </div>
 
       <div className={styles.element}>
         <p className={styles.element_title}>Who will win</p>
-        <div className={styles.element_block}></div>
-        <div className={styles.element_block2}></div>
-        <p className={styles.element_paragraph}>Your prediction, your game - vote!</p>
+
+        <div className={styles.element_container}>
+          <div className={styles.element_block}></div>
+
+          <div className={styles.element_block2}></div>
+        </div>
+
+        <p className={styles.element_paragraph}>
+          Your prediction, your game - vote!
+        </p>
       </div>
 
       <div className={styles.element}>
         <p className={styles.element_title}>Who will win</p>
-        <div className={styles.element_block}></div>
-        <div className={styles.element_block2}></div>
-        <p className={styles.element_paragraph}>Your prediction, your game - vote!</p>
+
+        <div className={styles.element_container}>
+          <div className={styles.element_block}></div>
+
+          <div className={styles.element_block2}></div>
+        </div>
+        <p className={styles.element_paragraph}>
+          Your prediction, your game - vote!
+        </p>
       </div>
 
       <div className={styles.element}>
         <p className={styles.element_title}>Who will win</p>
-        <div className={styles.element_block}></div>
-        <div className={styles.element_block2}></div>
-        <p className={styles.element_paragraph}>Your prediction, your game - vote!</p>
+
+        <div className={styles.element_container}>
+          <div className={styles.element_block}></div>
+
+          <div className={styles.element_block3}></div>
+          
+          <div className={styles.element_block2}></div>
+        </div>
+
+        <p className={styles.element_paragraph}>
+          Your prediction, your game - vote!
+        </p>
       </div>
     </div>
   );
