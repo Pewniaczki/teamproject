d297179471de94fed06e0bed2092f8cd5b8b33f7
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-08 18:29:19 +0200

    fix: update homepage in package.json and base path in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/team_project_frontend/',
+  base: '/teamproject/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
