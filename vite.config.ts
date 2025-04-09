be14cb95f195a293c876f43831608b7813f5e9ff
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-09 16:45:42 +0200

    fix: simplify base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) => ({
-  base: mode === 'production' ? '/teamproject/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
