ae2d22c9c882274675e97a8067dff27c3af8e856
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-10 17:50:12 +0200

    fix: add predeploy script to package.json and comment out base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  base: '/teamproject/',
+  // base: '/teamproject/',
   plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
