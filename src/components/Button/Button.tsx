13fa914aa559f15d153ab16fdd6691a2fd83d578
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-24 18:56:02 +0200

    refactor: comment out unused Button.scss import and enhance button styling for consistency

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,13 @@
-import './Button.scss';
+// import './Button.scss';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 export const Button = ({ children, ...buttonProps }: ButtonProps) => {
   return (
-    <button className="button" {...buttonProps}>
+    <button
+      className="w-full cursor-pointer rounded-lg border-1 border-[var(--color-grey-50)] bg-[var(--color-grey-70)] p-3 font-bold text-[var(--color-grey-20)] transition-[background-color,transform] duration-300 ease-in-out hover:-translate-y-0.5 hover:border-1 hover:border-[var(--color-secondary)] hover:bg-[var(----color-secondary-20)] hover:text-[var(--color-secondary)] active:translate-y-0 disabled:transform-none disabled:cursor-not-allowed disabled:opacity-50"
+      {...buttonProps}
+    >
       {children}
     </button>
   );
