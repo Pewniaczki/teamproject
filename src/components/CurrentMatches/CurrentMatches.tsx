7e8117993a6c3af6855c92de6f02934b84872488
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-25 20:20:05 +0200

    refactor: replace SCSS styles with Tailwind CSS in CurrentMatches component for improved consistency

--- src/components/CurrentMatches/CurrentMatches.tsx
+++ src/components/CurrentMatches/CurrentMatches.tsx
@@ -1,4 +1,3 @@
-import styles from './CurrentMatches.module.scss';
 import SoccerBall from '../../assets/icons/soccer-ball.svg';
 import { useNavigate } from 'react-router-dom';
 import { Match } from '../../types/countryMatchesTypes';
@@ -11,7 +10,7 @@ export const CurrentMatches: React.FC<Props> = ({ matches }) => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.container}>
+    <div className="no-scrollbar flex w-full max-w-175 flex-wrap justify-center gap-2 overflow-x-auto lg:flex-col">
       {matches.map((match, index) => (
         <div
           onClick={() =>
@@ -20,33 +19,39 @@ export const CurrentMatches: React.FC<Props> = ({ matches }) => {
             })
           }
           key={index}
-          className={styles.card}
+          className="flex shrink-0 flex-col items-center justify-center gap-2 rounded-sm bg-[var(--color-grey-70)] px-4 py-2 text-center text-[var(--color-grey-20)] lg:flex-row lg:justify-between"
         >
-          <div className={styles.teams}>
+          <div className="flex items-start justify-center lg:gap-1">
             {
               <img
                 src={match.match_info.home_team_logo}
                 alt={match.match_info.home_team}
-                className={styles.icon}
+                className="h-4 w-4 lg:h-7 lg:w-7 lg:translate-y-[50%]"
               />
             }
 
             <img
               src={SoccerBall}
               alt="soccer ball"
-              className={styles.icon__soccer}
+              className="h-8 w-8 lg:h-14 lg:w-14"
             />
             {
               <img
                 src={match.match_info.away_team_logo}
                 alt={match.match_info.away_team}
-                className={styles.icon}
+                className="h-4 w-4 lg:h-7 lg:w-7 lg:translate-y-[50%]"
               />
             }
           </div>
 
-          <div className={styles.info}>
-            <span>{match.match_info.date_time.split('T')[1].split(':').slice(0,2).join(':')}</span>
+          <div className="flex flex-col text-sm leading-3.5 font-medium lg:flex-row lg:gap-4">
+            <span>
+              {match.match_info.date_time
+                .split('T')[1]
+                .split(':')
+                .slice(0, 2)
+                .join(':')}
+            </span>
           </div>
         </div>
       ))}
