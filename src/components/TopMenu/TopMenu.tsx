103c646894b7b9654d42229d643a4dbf74726d63
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-04-27 08:34:22 +0200

    refactor: replace SCSS styling with Tailwind CSS in TopMenu component for improved consistency

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,8 +1,5 @@
-import cn from 'classnames';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
-import style from './TopMenu.module.scss';
 
 export const TopMenu: React.FC = () => {
   const links = [
@@ -14,24 +11,25 @@ export const TopMenu: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <nav className={style.nav}>
-      <div className={style.nav__active}>
+    <nav
+      id="nav-component"
+      className="after relative mb-2.5 flex lg:mb-8 lg:w-full lg:px-2.5 lg:py-0"
+    >
+      <div className="mr-[4%] lg:w-[calc(160px+4%)]">
         <Link
-          className={`${style.nav__active__link} ${style.nav__active}`}
+          className={`mr-[4%] leading-10 font-bold no-underline lg:text-3xl`}
           to={links[activeIndex][0]}
         >
           {links[activeIndex][1]}
         </Link>
       </div>
 
-      <div className={style.nav__container}>
+      <div className="relative flex w-full gap-x-[18%] overflow-auto pr-6.5 lg:justify-evenly lg:p-0">
         {links.map((link, index) => (
           <Link
             onClick={() => setActiveIndex(index)}
             key={link[0]}
-            className={cn(style.nav__container__link, {
-              [style.hidden]: activeIndex === index,
-            })}
+            className={`font-boldtext-[var(--color-grey-50)] leading-10 no-underline lg:text-3xl lg:text-[var(--color-grey-30)] ${activeIndex === index && 'hidden'} )`}
             to={link[0]}
           >
             {link[1]}
