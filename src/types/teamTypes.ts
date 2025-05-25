8964922c9089390f546fb3e1ce0e93f1d6ad7322
Patryk-Buczkowski <132567382+Patryk-Buczkowski@users.noreply.github.com>
2025-05-25 08:44:09 +0200

    Patryk (#20)
    * fix: adjust search icon visibility for mobile view in Options component
    
    * refactor: remove unused SCSS variables and imports from main.scss
    
    * feat: implement search functionality in MatchesPage and enhance SearchBar component
    
    * fix: improve loading state display and enhance search result item styling in MatchesPage
    
    * fix: add cursor pointer to CurrentMatchCard for better UX
    
    * fix: remove SearchBar component from CurrentMatch for improved layout
    
    * refactor: remove unused imports and break point listeners in CurrentMatch component
    
    * feat: add LeaguesPage and Country components with loading state management
    
    * fix: update authentication handling to use sessionStorage and improve logout functionality
    
    * feat: implement user authentication checks and improve logout functionality; add TeamTypes definition
    
    * fix: update match ID reference in betWinner function to use currentMatch prop
    
    * feat: implement betting store for match ID management and update CurrentDetails component to utilize it
    
    * feat: add removeMatchId function to betting store and update CurrentDetails component to utilize it
    
    * fix: change axios request method from GET to POST for user authentication
    
    * fix: refactor handleItemClick to await handleLogOut and remove unnecessary reload
    
    * fix: refactor authentication logic to use axios GET request for user status check
    
    * fix: refactor betting store types and improve match ID handling

--- src/types/teamTypes.ts
+++ src/types/teamTypes.ts
@@ -1,22 +1,24 @@
 export type TeamTypes = {
-        team_id: 33,
-        last_five_matches: [
-            {
-                home_team: string,
-                home_team_logo: string,
-                home_score: 0,
-                away_team: string,
-                away_team_logo: string,
-                away_score: 2,
-                date_and_time: string
-            },
-        ],
-        code: string,
-        name: string,
-        logo: string,
-        league: number,
-        country: string
-    }
+  team_id: 33;
+  last_five_matches: [
+    {
+      home_team: string;
+      home_team_logo: string;
+      home_score: 0;
+      away_team: string;
+      away_team_logo: string;
+      away_score: 2;
+      date_and_time: string;
+    },
+  ];
+  code: string;
+  name: string;
+  logo: string;
+  league: number;
+  country: string;
+};
 
-    export type TeamType = '1' | '2';
-
+export type TeamType = '1' | '2';
+export type ScoreType = 'yes' | 'no';
+export type FirstType = '1f' | '2f' | 'wh';
+export type DrawType = 'yesD' | 'noD';
