f8362773983e7ed06ef18e78dde81673c1c4b74d
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-06-26 20:42:42 +0200

    feat: refactor test component layout for improved readability

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -2,14 +2,22 @@ export const test: React.FC = () => {
   return (
     <>
       <p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
-      <span>elo</span><p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
+      <span>elo</span>
+      <p>test</p>
       <span>elo</span>
     </>
   );
