e3c3ffeb39418f0b1b38145616e82b4501b1b738
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-06-26 20:39:02 +0200

    feat: add additional content to test component

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -2,6 +2,8 @@ export const test: React.FC = () => {
   return (
     <>
       <p>test</p>
+      <span>elo</span><p>test</p>
+      <span>elo</span><p>test</p>
       <span>elo</span>
     </>
   );
