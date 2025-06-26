5ac5c4ed5efee2918f89b73c2e440d523373db9f
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-06-26 20:58:12 +0200

    feat: remove duplicate elements from test component for cleaner output

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -9,9 +9,7 @@ export const test: React.FC = () => {
       <span>elo</span>
       <p>test</p>
       <span>elo</span>
-      <p>test</p>
-      <span>elo</span>
-    
+      <p>kupa</p>    
     </>
   );
 };
