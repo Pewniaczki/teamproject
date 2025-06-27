c9487e6bef74f0c00bc641640c81b42a1a95cf19
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-06-27 07:29:41 +0200

    feat: add dynamic rendering of array elements in test component

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,6 +1,10 @@
 export const test: React.FC = () => {
+  const array = ['fff', 'gg', 'hhh', 'jj'];
   return (
     <>
+    {array.map(item => 
+      <p>{item}</p>
+    )}
       <p>test</p>
       <span>elo</span>
       <p>test</p>
